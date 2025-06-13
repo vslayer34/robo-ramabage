ea49946dcfd2783eafdb98970ce57dd125cbceb2
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2025-06-13 15:18:31 +0300

    Added Attack and player detection to the enemy

--- Scripts/Enemies/Enemy.cs
+++ Scripts/Enemies/Enemy.cs
@@ -1,4 +1,5 @@
 using Godot;
+using RoboRamabage.Scripts.Helper.Animations;
 using RoboRamabage.Scripts.Helper.Groups;
 using RoboRampage.Player;
 
@@ -10,6 +11,9 @@ public partial class Enemy : CharacterBody3D
 	[Export]
 	private NavigationAgent3D _navAgent;
 
+	[Export]
+	private AnimationPlayer _animePlayer;
+
 	private Player.Player _playerNode;
 
 	public const float Speed = 5.0f;
@@ -20,24 +24,30 @@ public partial class Enemy : CharacterBody3D
 	[Export]
 	private float _aggroDistance = 12.0f;
 
+	[Export]
+	private float _attackRange = 1.5f;
 
 
-    // Game Loop Methods---------------------------------------------------------------------------
+
+	// Game Loop Methods---------------------------------------------------------------------------
 
 	public override void _Ready()
 	{
 		_playerNode = GetTree().GetFirstNodeInGroup(GroupNames.PLAYER) as Player.Player;
 	}
 
-    public override void _Process(double delta)
-    {
-        _navAgent.TargetPosition = _playerNode.GlobalPosition;
-    }
+	public override void _Process(double delta)
+	{
+		_navAgent.TargetPosition = _playerNode.GlobalPosition;
+	}
 
 
 	public override void _PhysicsProcess(double delta)
 	{
-		_isTriggered = GlobalPosition.DistanceTo(_playerNode.GlobalPosition) <= _aggroDistance;
+		var _distanceToPlayer = GlobalPosition.DistanceTo(_playerNode.GlobalPosition);
+
+		_isTriggered = _distanceToPlayer <= _aggroDistance;
+
 
 		var nextPosition = _navAgent.GetNextPathPosition();
 		Vector3 velocity = Velocity;
@@ -50,9 +60,15 @@ public partial class Enemy : CharacterBody3D
 
 		Vector3 direction = _isTriggered ? GlobalPosition.DirectionTo(nextPosition) : Vector3.Zero;
 
+		if (_isTriggered && _distanceToPlayer <= _attackRange)
+		{
+			_animePlayer.Play(AnimationNames.Enemy.ATTACK);
+		}
+
 
 		if (direction != Vector3.Zero)
 		{
+			LookTowardDestination(direction);
 			velocity.X = direction.X * Speed;
 			velocity.Z = direction.Z * Speed;
 		}
@@ -64,5 +80,21 @@ public partial class Enemy : CharacterBody3D
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+
+
+	private void LookTowardDestination(Vector3 direction)
+	{
+		var directionWithoutYRotation = direction;
+		directionWithoutYRotation.Y = 0.0f;
+
+		// Added the global position because the direction is only local position
+
+		LookAt(GlobalPosition + directionWithoutYRotation, Vector3.Up, true);
+	}
+
+	private void Attack()
+	{
+		GD.Print("Attack Player");
 	}
 }
---
+++ Scripts/Helper/Animations/AnimationNames.cs
@@ -0,0 +1,8 @@
+namespace RoboRamabage.Scripts.Helper.Animations;
+public static class AnimationNames
+{
+    public static class Enemy
+    {
+        public const string ATTACK = "attack";
+    }
+}
