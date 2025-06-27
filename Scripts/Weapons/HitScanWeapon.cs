3f2cdf59d285e785b17a302e250300c51e69d48d
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2025-06-27 21:59:07 +0300

    Added Recoil and Raycast

--- Scripts/Weapons/HitScanWeapon.cs
+++ Scripts/Weapons/HitScanWeapon.cs
@@ -10,9 +10,25 @@ public partial class HitScanWeapon : Node3D
 	[Export]
 	private Timer _coolDownTimer;
 
+	[Export]
+	private Node3D _weaponMesh;
+
+	[Export]
+	private float _backwardRecoilForce = 0.05f;
+
+	private Vector3 _defaultMeshPosition;
+
+	[Export]
+	private RayCast3D _rayCaster;
 
 
-	// Game Loop Methods---------------------------------------------------------------------------
+
+    // Game Loop Methods---------------------------------------------------------------------------
+
+	public override void _Ready()
+	{
+		_defaultMeshPosition = _weaponMesh.Position;
+	}
 
 	public override void _Process(double delta)
 	{
@@ -20,9 +36,24 @@ public partial class HitScanWeapon : Node3D
 		{
 			if (_coolDownTimer.IsStopped())
 			{
-				_coolDownTimer.Start(1.0f / fireRatePerSecond);
-				GD.Print("shot fired");
+				Shoot();
 			}
+
+			_weaponMesh.Position = _weaponMesh.Position.Lerp(_defaultMeshPosition, (float)delta * 10.0f);
 		}
-    }
+	}
+
+	// Member Methods------------------------------------------------------------------------------
+
+	private void Shoot()
+	{
+		_coolDownTimer.Start(1.0f / fireRatePerSecond);
+		GD.Print("shot fired");
+		GD.Print(_rayCaster.GetCollider());
+
+		Vector3 currentWeaponPosition = _weaponMesh.Position;
+		currentWeaponPosition.Z += _backwardRecoilForce;
+		
+		_weaponMesh.Position = currentWeaponPosition;
+	}
 }
